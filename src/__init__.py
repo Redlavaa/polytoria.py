2ba245e6d11ef88d1fa6713329353ce91e801f32
Redlavaa <98986258+Redlavaa@users.noreply.github.com>
2026-05-19 17:59:26 -0500

    Added pyproject

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,3 @@
-# main.py
-import asyncio
-from src.wrapper import Polytoria
+from .wrapper import Polytoria
 
-async def main():
-
-    polytoria = Polytoria(browser="chrome") 
-    
-    user = await polytoria.user(22783)
-    print(user.username)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+__all__ = ["Polytoria"]
